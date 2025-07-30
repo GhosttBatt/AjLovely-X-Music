a4cad141e506fa9e1a74f8f04d859f62dd5e1cfe
GhosttBatt <ghosttbatt@gmail.com>
2025-07-30 19:19:57 +0530

    AJconfig.py

--- config.py
+++ config.py
@@ -10,15 +10,15 @@ from thumbnails import *
 from fonts import *
 
 # Use getenv for all sensitive/configurable values
-API_ID = os.getenv("API_ID","")
-API_HASH = os.getenv("API_HASH", "")
-STRING_SESSION = os.getenv("STRING_SESSION", "")
-GROUP = os.getenv("GROUP", "nub_coder_s")
-CHANNEL = os.getenv("CHANNEL", "nub_coders_updates")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "")
-OWNER_ID = int(os.getenv("OWNER_ID", 6076474757))
+API_ID = os.getenv("API_ID","26204186")
+API_HASH = os.getenv("API_HASH", "c277a7f93583f68d0fdfdcb68f5fc6c0")
+STRING_SESSION = os.getenv("STRING_SESSION", "BQGP2BoAP4JT-5wna5L_Z1nareUi02O8FEyUUn4VmOSrh71vWKcJoqazyJ4_LmSMMKR-9V7-3_QLUUa0YP02Nctpdu0v4ZdqiE-KldC1NtGYVloBBxF0lXMHlSGXstx5crc19uDGrnJiTs809TF70PkewPb9iHSp1mfTGI0nY-uzhJHgoKiyLlUgjWsLnDuNw66RQo2TrbAiNK0ZtGj1qfe7dTDYF0FFrh4gK7DkWck3B7KeGNNfEred5YRUOPTHp0OEq28cwNGHQNI6nVrdqDLDnZNV1xAtXhVoyTYYiZ12-tDjblYm8PX9l72JJluqvpyG7Pjarp8AAmc3WlPaqOWZJ32_YQAAAAHAItULAA")
+GROUP = os.getenv("GROUP", "HeartBeat_Fam")
+CHANNEL = os.getenv("CHANNEL", "HeartBeat_Offi")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8209706073:AAGayoWtC1Dqch1UbjAj8_-fo3GCbMrmZmI")
+OWNER_ID = int(os.getenv("OWNER_ID", 8099828972))
 LOGGER_ID = os.getenv("LOGGER_ID", None)
-mongodb = os.getenv("MONGODB_URI", "mongodb+srv://nubcoders:nubcoders@music.8rxlsum.mongodb.net/?retryWrites=true&w=majority&appName=music")
+mongodb = os.getenv("MONGODB_URI", "mongodb+srv://ghosttbatt:Ghost2021@ghosttbatt.ocbirts.mongodb.net/?retryWrites=true&w=majority")
 # Working directory
 ggg = os.getcwd()
 
