78dd0f192f9084177bf8a0751f26aca40c99096d
GhosttBatt <ghosttbatt@gmail.com>
2025-08-01 12:53:18 +0530

    AJconfig.py

--- config.py
+++ config.py
@@ -12,10 +12,10 @@ from fonts import *
 # Use getenv for all sensitive/configurable values
 API_ID = os.getenv("API_ID","26204186")
 API_HASH = os.getenv("API_HASH", "c277a7f93583f68d0fdfdcb68f5fc6c0")
-STRING_SESSION = os.getenv("STRING_SESSION", "BQGP2BoAP4JT-5wna5L_Z1nareUi02O8FEyUUn4VmOSrh71vWKcJoqazyJ4_LmSMMKR-9V7-3_QLUUa0YP02Nctpdu0v4ZdqiE-KldC1NtGYVloBBxF0lXMHlSGXstx5crc19uDGrnJiTs809TF70PkewPb9iHSp1mfTGI0nY-uzhJHgoKiyLlUgjWsLnDuNw66RQo2TrbAiNK0ZtGj1qfe7dTDYF0FFrh4gK7DkWck3B7KeGNNfEred5YRUOPTHp0OEq28cwNGHQNI6nVrdqDLDnZNV1xAtXhVoyTYYiZ12-tDjblYm8PX9l72JJluqvpyG7Pjarp8AAmc3WlPaqOWZJ32_YQAAAAHAItULAA")
+STRING_SESSION = os.getenv("STRING_SESSION", "BQGP2BoAny3jHaKC1IiWLUMScYpYEXgPHmkXtz9M0CihxUGngp1cqNFuixfYyNNToLGVe4LOfqQPMEhZ1iAEbLgvRXJ7tR-IEeoGmaxFQ8M96zpxzYxYgPMQyOlJ84tS_eA4EGX8AUNQqow1RVpwU9fBLZiYzQ0Hfd1Lxo3r9hAM3CqHbEtvLvbOmcnI_X0bb8iqIsUcM2bPh8tB9M8lER9mbZz1ApxccwlG_x0Uu5nPqvD5_ze3x9IWQQ3pIngx5yGd0huDRGHLDPtEHzonIz8TCEv96rKfuXdrkif8fj1DeKXMhuxFGX29pWL3WlOCKFqneDtFM1GzYPGTovS0VYiIIjZmIQAAAAHAItULAA")
 GROUP = os.getenv("GROUP", "HeartBeat_Fam")
 CHANNEL = os.getenv("CHANNEL", "HeartBeat_Offi")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "8209706073:AAGayoWtC1Dqch1UbjAj8_-fo3GCbMrmZmI")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8209706073:AAH8stU0NeX1IoqTdfAzfVTBQVOzDU5nHDc")
 OWNER_ID = int(os.getenv("OWNER_ID", "8099828972"))
 LOGGER_ID = os.getenv("LOGGER_ID", "2275616383")
 mongodb = os.getenv("MONGODB_URI", "mongodb+srv://ghosttbatt:Ghost2021@ghosttbatt.ocbirts.mongodb.net/?retryWrites=true&w=majority")
